0ee923223694b6059c5633f4e260cff6df1b38ed
fallnomega <fallnomega@gmail.com>
2023-02-13 12:30:57 -0800

    game just about complete

--- main.py
+++ main.py
@@ -8,40 +8,39 @@ screen.register_shape(new_shape)
 turtle.shape(new_shape)
 data = pandas.read_csv("50_states.csv")
 data_dict = data.to_dict()
-# state_picked = turtle.Turtle()
-# screen.textinput( "Guess a State","Guess the name of a state in the United States:   ")
-# state_picked.shape("circle")
-# state_picked.shapesize(.1,.1,.1)
-# state_picked.penup()
-
+all_states = data['state'].tolist()
+player_score = 0
+correct_guesses = []
 game_on = True
+
 while game_on:
-    user_input = screen.textinput("Guess a State", "Guess the name of a state in the United States:   ").lower()
-    if user_input==None:
-        exit()
+    user_input = screen.textinput(f"Correctly Guessed States {player_score}/50",
+                                  "Guess the name of a state in the United States:   ")
+
+    if user_input == 'exit':
+        states_to_learn = []
+        for x in all_states:
+            if x not in correct_guesses:
+                states_to_learn.append(x)
+        print(states_to_learn)
+        pd_temp = pandas.DataFrame(states_to_learn)
+        pd_temp.to_csv("states_to_learn.csv")
+        break
     for x in range(0, len(data_dict["state"])):
         lowered = data_dict["state"][x]
         lowered = lowered.lower()
-        if lowered == user_input:
-            print(f"{data_dict['state'][x]} WINNNER")
-            print(data_dict['x'][x])
-            print(data_dict['y'][x])
+        if lowered == user_input.lower():
             apply_state = turtle.Turtle()
+            apply_state.hideturtle()
             apply_state.shape("circle")
-            apply_state.shapesize(.1,.1,.1)
+            apply_state.shapesize(.1, .1, .1)
             apply_state.penup()
-            apply_state.goto(data_dict['x'][x],data_dict['y'][x])
+            apply_state.goto(data_dict['x'][x], data_dict['y'][x])
             apply_state.write(data_dict['state'][x])
+            player_score += 1
+            correct_guesses.append(data_dict['state'][x])
 
         else:
             continue
 
-# def get_mouse_click_coor(x,y):
-#     print(x,y)
-# screen.onscreenclick(get_mouse_click_coor)
 screen.mainloop()
-
-# screen.exitonclick()
-
-
-
